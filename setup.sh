617135f7c8ecd194b601ce468ab130845a01e255
praveenreddi <30587635+praveenreddi@users.noreply.github.com>
2020-06-21 15:16:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"haritshandilya198@gmail.com\"\n\
+email = \"reddypraveen722@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
